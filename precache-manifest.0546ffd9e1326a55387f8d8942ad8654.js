8bfa6bd46f22ce8b0bb20e80ad172b875fe0cfe0
Maberton <Maberton@users.noreply.github.com>
2022-01-05 23:58:57 +0000

    deploy: 95add9479b1fca46051293fd9bbd40af82c17544

---
+++ precache-manifest.0546ffd9e1326a55387f8d8942ad8654.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504b2b03ace274b91aaad4e125343b27",
+    "url": "https://maberton.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "fad5ee96f4f0bc4bf159",
+    "url": "https://maberton.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "e797242095453c4194ee",
+    "url": "https://maberton.github.io/task-management-frontend/static/js/2.c9ebd30d.chunk.js"
+  },
+  {
+    "revision": "fad5ee96f4f0bc4bf159",
+    "url": "https://maberton.github.io/task-management-frontend/static/js/main.05bfbe26.chunk.js"
+  },
+  {
+    "revision": "5e002c82984b48a5b320",
+    "url": "https://maberton.github.io/task-management-frontend/static/js/runtime~main.4fb3bf66.js"
+  }
+]);
